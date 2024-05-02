1fe950307518d0e8b6787c8ae68caf656666b69b
sammmms <samuelonasis23@gmail.com>
2024-05-02 10:27:45 +0700

    fix: shouldRemove condition feat: stop showing mention list when cursor is in a mentioned text

--- lib/pages/issue/widgets/issue_detail/jention/jention_controller.dart
+++ lib/pages/issue/widgets/issue_detail/jention/jention_controller.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mentions/flutter_mentions.dart';
 
 class MentionedItem {
   String text;
@@ -35,21 +34,26 @@ class JentionEditingController extends TextEditingController {
       TextStyle? style,
       required bool withComposing}) {
     List<InlineSpan> listOfTextSpan = [];
-    
+
     // TODO: refactor and comment
     int cursor = 0;
     for (var i = 0; i < mentions.length; i++) {
       var mention = mentions[i];
       if (mention.startIndex != cursor) {
-        // create a normal text span before this this mention
+        // create a normal text span before this mention
         var normalText = text.substring(cursor, mention.startIndex);
         listOfTextSpan.add(normalSpan(normalText));
       }
-      var mentionText = text.substring(mention.startIndex, mention.endIndex + 1);
+      var mentionText =
+          text.substring(mention.startIndex, mention.endIndex + 1);
       listOfTextSpan.add(mentionSpan(mentionText));
       cursor = mention.endIndex + 1;
     }
-    if (cursor != text.length-1) {
+    // Readjust, there's no need of -1 since we want to add space if there are any
+    // Readjust, to <= since if the cursor somehow is greater than the text.length, we don't want to add any
+    // substring, since it DOES happen in some cases, for example adding a mention, and removing word by word
+    // creates this error where the cursor is greater than the text.length
+    if (cursor <= text.length) {
       var normalText = text.substring(cursor, text.length);
       listOfTextSpan.add(normalSpan(normalText));
     }
@@ -63,7 +67,7 @@ class JentionEditingController extends TextEditingController {
   }
 
   TextSpan mentionSpan(String string) {
-      return TextSpan(text: string, style: const TextStyle(color: Colors.blue));
+    return TextSpan(text: string, style: const TextStyle(color: Colors.blue));
   }
 
   @override
@@ -86,23 +90,43 @@ class JentionEditingController extends TextEditingController {
 
     if (value.text != newValue.text) {
       // preserve "preceeding mentions" + remove "intruded mentions" + adjust "traling mentions"
-      var s = value.selection.start;
-      var e = value.selection.end;
-      var lAdjust = newValue.text.length - value.text.length; // length
+      // Readjust variable name for easier readability
+      var cursorStart = value.selection.start;
+      var cursorEnd = value.selection.end;
+      var lengthAdjust = newValue.text.length - value.text.length; // length
 
       for (var i = mentions.length - 1; i >= 0; i--) {
         var mention = mentions[i];
-        var shouldRemove = e >= mention.startIndex && s <= mention.endIndex;
+        var shouldRemove = cursorEnd > mention.startIndex &&
+            cursorStart <= mention.endIndex + 1;
+        // Only remove if the end cursor is greater than the startIndex, example.
+        // cursorEnd = 6
+        // text = abcde @Sam \\ @Sam.startIndex = 6
+        // currently we're at |@
+        // when we made any changes at cursorEnd = 6 (previous cursor), we won't really affect the mention
+        // since pasting require the user to be at position 7, the same with deleting
+        // adding things in cursorEnd = 6, only affect the mention position to be adjusted
+        // ===================
+        // Another one is with the cursorStart <= mention.endIndex, this here causes a problem where
+        // If the cursor is on the endIndex, it doesn't remove the mention, example.
+        // cursorStart = 4
+        // text = @Sam
+        // currently we're at @Sam|
+        // removing any text after this should affect it, but since the logic before was cursorStart <= mention.endIndex
+        // We're only removing the mention IF the cursorStart <= mention.endIndex, which are 4 <= 3
+        // must remember, the cursorStart is the previous cursor position
+        // We need to adjust this, so that if our previous cursor position was 4 and we delete anything it
+        // would affect the condition so, we adjust the condition to cursorStart <= mention.endIndex + 1
         if (shouldRemove) {
           mentions.removeAt(i);
           printDatabase();
           continue;
         }
 
-        var shouldAdjust = mention.startIndex > s;
+        var shouldAdjust = mention.startIndex >= cursorStart;
         if (shouldAdjust) {
-          mention.startIndex = mention.startIndex + lAdjust;
-          mention.endIndex = mention.endIndex + lAdjust;
+          mention.startIndex = mention.startIndex + lengthAdjust;
+          mention.endIndex = mention.endIndex + lengthAdjust;
           printDatabase();
         }
       }
@@ -136,6 +160,15 @@ class JentionEditingController extends TextEditingController {
       return;
     }
 
+    // Check if cursor position is inside a mention -Sam
+    for (var mention in mentions) {
+      if (currentCursorPosition >= mention.startIndex &&
+          currentCursorPosition <= mention.endIndex + 1) {
+        _setMentionInfo(null);
+        return;
+      }
+    }
+
     int theStart = nearestPreceedingWhitespace + 1;
 
     final theText = text
@@ -160,7 +193,6 @@ class JentionEditingController extends TextEditingController {
 
     int from = start!;
     int to = value.selection.end;
-
     var newText = text.replaceRange(from, to, "@$name ");
     var selectionIndex = from + name.length + 1;
     value = TextEditingValue(
